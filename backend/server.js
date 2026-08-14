77ab95715e1f33b47c11bb0fb6c238989b7c6db7
lahari-tech25 <lahari7625@gmail.com>
2026-08-14 14:36:42 +0530

    Connected database and implemented user registeration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
+require('dotenv').config();
+
 const express = require('express');
+const connectDB = require('./db');
+const User = require('./models/User');
+
 const app = express();
 const port = 8080;
+
+//json  parser middleware 
+app.use(express.json());
+//connect the database
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send('Hello World from Auth system hey this is me  ');
 });
 
-app.get('/users',(req,res)=>{
-    res.send('User API endpoint');
-})
+
+
+app.post('/users/register',async (req,res)=>{
+    try{
+        const user = await User.create(req.body);
+        res.status(201).json({
+            message:"User created successfully",
+            user:user
+        });
+    }catch(error){
+        res.status(500).json({
+            message:"Failed to create user",
+            error:error.message
+        });
+    }
+});
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
